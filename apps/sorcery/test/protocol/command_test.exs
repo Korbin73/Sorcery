af1b809dde7de3992e9678c6fe983bd0f8509d16
Lee Bryant <elvie.bryant@gmail.com>
2017-06-22 14:21:55 -0700

    Started adding unit tests for genserver.

--- apps/sorcery/test/protocol/command_test.exs
+++ apps/sorcery/test/protocol/command_test.exs
@@ -1,5 +1,11 @@
 defmodule CommandTests do
   use ExUnit.Case, async: true
+  import ElixirLS.LanguageServer.Server
+
+  setup do
+    {:ok, lang_server} = start_link() 
+    {:ok, language_server: lang_server}
+  end
 
   test "Should return server capabilities" do
     :not_implemented
