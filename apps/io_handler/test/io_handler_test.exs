c98314237c15ae328fe903ca1a0010b0f13e3038
Lee Bryant <elvie.bryant@gmail.com>
2017-09-02 14:02:57 -0700

    Finished up the unit tests

--- apps/io_handler/test/io_handler_test.exs
+++ apps/io_handler/test/io_handler_test.exs
@@ -1,16 +1,25 @@
 defmodule IoHandlerTest do
   use ExUnit.Case
+  import ExUnit.CaptureIO
+
   doctest IoHandler
 
+  defmodule Stub do
+    def receive_packet(_packet) do
+      {:reply, :ok, %{}}
+    end
+  end
+
   test "Should send json to stdio" do
-    
+    result = IoHandler.send(%{"movie"=>"predator"})
+    assert(result == :ok, "this should fail")
   end
 
   test "Should return ok from process" do
-    
-  end
-
-  test "Should return ok from reading" do
-    
+    check_for_pid = fn
+      {:ok, pid} -> assert(is_pid(pid), "Did not return a pid")
+    end
+    IoHandler.start_link(Stub)
+    |> check_for_pid.()
   end
 end
